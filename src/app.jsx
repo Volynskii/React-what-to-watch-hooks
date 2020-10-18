1da09ef0f883eb3600655ec8ebf1d4419ea36dce
Антон <antonvolynskii@gmail.com>
2020-10-19 02:42:09 +1000

    add template for detail-page

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import MainPage from "./pages/main-page/main-page.jsx";
+import DetailsPage from "./pages/details-page/details-page";
+
+const getPage = () => {
+  switch (location.pathname) {
+    case `/details`:
+      return <DetailsPage />;
+    default:
+      return <MainPage />;
+  }
+};
 
 const App = () => {
-  return <MainPage />;
+  return getPage();
 };
 
 export default App;
