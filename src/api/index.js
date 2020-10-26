9314dfc53d55763e8acbd6e902d0a6ad36be3959
Антон <antonvolynskii@gmail.com>
2020-10-27 00:03:24 +1000

    reducer for movie-details

--- src/api/index.js
+++ src/api/index.js
@@ -9,6 +9,17 @@ export const api = {
   auth: {
     login(credentials) {
       return fetch.post(`/login`, credentials);
+    },
+    check() {
+      return fetch.get(`/login`);
+    }
+  },
+  comments: {
+    fetch(id) {
+      return fetch.get(`/comments/${id}`);
+    },
+    create(id, comment) {
+      return fetch.post(`/comments/${id}`, comment);
     }
   }
 };
