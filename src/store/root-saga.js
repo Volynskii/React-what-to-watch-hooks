eed8fcd4d5488609757d7bc942d33a1ff7be2037
Антон <antonvolynskii@gmail.com>
2020-10-26 22:35:08 +1000

    add routing

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
+import { watchAuth } from "./auth/saga/watchers";
 import { watchMovies } from "./movies/saga/watchers";
 
 export function* rootSaga() {
 
-  yield all([call(watchMovies)]);
+  yield all([call(watchAuth), call(watchMovies)]);
 }
